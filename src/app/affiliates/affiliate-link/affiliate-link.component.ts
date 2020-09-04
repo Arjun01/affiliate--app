10f68004fe0d2a6149c651688896e478086654e2
Arjun Sharma <erarjun0@gmail.com>
2020-09-04 16:50:36 +0530

    copying text from clipboard

--- src/app/affiliates/affiliate-link/affiliate-link.component.ts
+++ src/app/affiliates/affiliate-link/affiliate-link.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-affiliate-link',
   templateUrl:'./affiliate-link.component.html'
 })
 export class AffiliateLinkComponent{
+  newLink = 'https://smartckts.com/register/?affiliates=69';
 
+  onCopyLink(inputElement: HTMLInputElement){
+    console.dir(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    alert('Link copied');
+  }
 }

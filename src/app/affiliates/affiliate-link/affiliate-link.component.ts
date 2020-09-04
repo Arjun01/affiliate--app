fcd2b554fdd4efd5fb7b6e74e16ffe163470c021
Arjun Sharma <erarjun0@gmail.com>
2020-09-04 15:27:47 +0530

    creating affiliate-link component

---
+++ src/app/affiliates/affiliate-link/affiliate-link.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-affiliate-link',
+  templateUrl:'./affiliate-link.component.html'
+})
+export class AffiliateLinkComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AffiliateLinkComponent } from './affiliates/affiliate-link/affiliate-link.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AffiliateLinkComponent
   ],
   imports: [
     BrowserModule,
